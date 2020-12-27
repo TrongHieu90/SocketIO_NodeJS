dffa4284ac8a2f79a21026a109789a01fc52fc4a
Trong Hieu <hieu.nguyen@amanotes.com>
2020-12-27 11:42:14 +0700

    + Add socket.io

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const port = 8080;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
+app.get('/', (req, res) => res.sendFile(__dirname + 'public/index.html'));
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -37,5 +39,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port , ()=> console.log('Connected'));
 
 module.exports = app;
